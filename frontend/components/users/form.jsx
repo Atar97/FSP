7204bdff81530e22b059016b2eacfe44e2a3b47d
Austin <austin.cotant@gmail.com>
2018-10-09 13:22:52 -0700

    Add signup form

--- frontend/components/users/form.jsx
+++ frontend/components/users/form.jsx
@@ -22,6 +22,8 @@ class UserForm extends React.Component {
     //   lname: 'Last name'
     // };
     this.handleChange = this.handleChange.bind(this);
+    this.createSelect = this.createSelect.bind(this);
+    this.createMonthSelect = this.createMonthSelect.bind(this);
   }
 
   handleChange(fieldName) {
@@ -29,33 +31,75 @@ class UserForm extends React.Component {
   }
 
   handleSubmit(event) {
-    // debugger;
     event.preventDefault();
     const DOB = `${this.state.day}/${this.state.month}/${this.state.year}`;
     const user = {user: Object.assign({}, this.state, {DOB})};
     this.props.sendForm(user);
   }
 
+  createSelect(min, max, name) {
+    const mappableArray = [];
+    for (let i = min; i <= max; i++) {
+      mappableArray.push(i);
+    }
+    return (
+      <select onChange={this.handleChange(name)}>
+        <option>{name}</option>
+        {mappableArray.map(el => {
+          return <option key={el} value={el}>{el}</option>;
+        })}
+      </select>
+    );
+  }
+
+  createMonthSelect() {
+    const mappableArray = [1,2,3,4,5,6,7,8,9,10,11,12];
+    const months = [null,
+          'January', 'February', 'March',
+          'April', 'May', 'June',
+          'July', 'August', 'September',
+          'October', 'November', 'December'];
+    return (
+      <select onChange={this.handleChange('month')}>
+        <option>Month</option>
+        {mappableArray.map(el => {
+          return <option key={el} value={el}>{months[el]}</option>;
+        })}
+      </select>
+    );
+  }
+
   render() {
     if (this.props.formType === 'login') {
       return (
-        <div>
+        <div className='userform login-form'>
           <Link to='/signup'>sign up</Link>
           <form onSubmit={this.handleSubmit.bind(this)}>
             <input onChange={this.handleChange('email')}></input>
-            <input onChange={this.handleChange('password')}></input>
+            <input type='password'
+              onChange={this.handleChange('password')}></input>
             <button>Log In</button>
           </form>
         </div>
       );
     }
     return (
-      <div>
+      <div className='userform signup-form'>
         <Link to='/login'>log in</Link>
+          <form onSubmit={this.handleSubmit.bind(this)}>
+            <input onChange={this.handleChange('fname')}></input>
+            <input onChange={this.handleChange('lname')}></input>
+            <input onChange={this.handleChange('email')}></input>
+            <input type='password'
+              onChange={this.handleChange('password')}></input>
+            {this.createSelect(1, 31, 'day')}
+            {this.createMonthSelect()}
+            {this.createSelect(1900, 2006, 'year')}
+            <button>Sign Up</button>
+          </form>
       </div>
     );
   }
-
 
 }
 
