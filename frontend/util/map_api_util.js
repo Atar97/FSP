ce26ab2ecde5df18701c9122a956a608e466374a
Austin <austin.cotant@gmail.com>
2018-10-17 14:38:44 -0700

    Security

--- frontend/util/map_api_util.js
+++ frontend/util/map_api_util.js
@@ -1,14 +1,14 @@
 // 1600+Amphitheatre+Parkway,+Mountain+View,+CA address style
 export const fetchLocation = addressStr => (
   $.ajax({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${addressStr}&key=AIzaSyDfzkfK4BYKYvNKWtxv6OVb3dtTw_ML3qw`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${addressStr}&key=${window.tsoachick}`,
     method: 'GET',
   })
 );
 
 export const fetchAddress = latLng => (
   $.ajax({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latLng}&key=AIzaSyDfzkfK4BYKYvNKWtxv6OVb3dtTw_ML3qw`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latLng}&key=${window.tsoachick}`,
     method: 'GET'
   })
 );
